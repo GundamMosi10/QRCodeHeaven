c9719a14000b73c0861ff2cf85e103c3d859dd6a
Colin Mosi Cargile <cargile10@gmail.com>
2022-08-16 21:12:06 -0400

    fixed the issue with the save button not showing up in the UI on render

--- script.js
+++ script.js
@@ -36,29 +36,36 @@ const generateQRCode = (url, size) => {
     });
 };
 
-const showSpinner = () => {
-    document.getElementById('spinner').style.display = 'block';
-};
-const hideSpinner = () => {
-    document.getElementById('spinner').style.display = 'none';
-};
-
 const clearUI = () => { //function to clear the UI or previously created QR code
     qr.innerHTML = '';
-    const saveLink = document.getElementById('save-link');
-    if (saveLink) saveLink.remove();
+    const saveBtn = document.getElementById('save-link');
+    if (saveBtn) {
+        saveBtn.remove();
+    }
 };
 
+const showSpinner = () => {
+    const spinner = document.getElementById('spinner');
+    spinner.style.display = 'block';
+};
+
+const hideSpinner = () => {
+    const spinner = document.getElementById('spinner');
+    spinner.style.display = 'none';
+};
+
+// Create save button to download QR code as image
 const createSaveBtn = (saveUrl) => {
     const link = document.createElement('a');
     link.id = 'save-link';
-    link.classlist = 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+    link.classList =
+      'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
     link.href = saveUrl;
     link.download = 'qrcode';
     link.innerHTML = 'Save Image';
     document.getElementById('generated').appendChild(link);
 };
-
+  
 hideSpinner();
 
 form.addEventListener('submit', onGenerateSubmit);
